c4d4396ba8e9d720a6ff462543b23b118d585223
bodofia olinga <bodifolinga@gmail.com>
2025-11-27 22:42:54 +0100

    mise au point du model

--- src/main/java/com/meteo/model/Meteo.java
+++ src/main/java/com/meteo/model/Meteo.java
@@ -1,1 +1,22 @@
-
+package com.meteo.model;  public class Meteo {
+    private String description;
+    private double temperature;
+    private int humidite;
+    private String city;
+    public Meteo(String description, double temperature, int humidite, String city) {
+        this.description = description;
+        this.temperature = temperature;
+        this.humidite =  humidite;
+        this.city = city;     }
+    public String getDescription() {return description;     }
+    public double getTemperature() {         return temperature;     }
+    public int getHumidite() {         return humidite;     }
+    public String getCity() {         return city;     }
+    @Override
+    public String toString(){
+        return "description :"+ description + "\n" +
+                "temperature :"+ temperature+ "°C"+ "\n"+
+                "humiditer :" + humidite + "\n" +
+                "ville :" + city;
+    }
+}
